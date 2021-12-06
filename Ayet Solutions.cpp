5c97b8a4ace0a3175b74741764ecf31bf339484b
CringeKing93 <55989895+CringeKing93@users.noreply.github.com>
2021-12-06 11:52:50 +0100

    Update Ayet Solutions.cpp

--- Ayet Solutions.cpp
+++ Ayet Solutions.cpp
@@ -109,23 +109,62 @@ Hard questions(build the relations requiered)*/
 
 
 1 - /*Get all exams for a specific Course*/
+select * from exams 
+where exam_course_code in ('CS_SOFTWARE_SECURITY', 'exam_type'); 
 
+
+select e.exam_course_code, e.exam_weight, e.exam_type from exams e
+where e.exam_course_code in 'SE_ADV_JAVA'
 
 
 2 - /*Get all Grades for a specific Student*/
-
+select * from grades
+where grade_student_epita_email_ref  in ('kallie.blackwood@epita.fr', 'grade_score')
+  
+select c.contact_first_name,c.contact_last_name, g.grade_course_code_ref, g.grade_score from grades g
+inner join students s on g.grade_student_epita_email_ref = s.student_epita_email 
+inner join contacts c on s.student_contact_ref = c.contact_email 
+where g.grade_student_epita_email_ref='kallie.blackwood@epita.fr'
 
 3 - /*Get the final grades for a student on a specifique course or all courses*/
+select * from courses
+select * from grades
+
+
+select g grade_student_epita_email_ref, grade_score from grades g  in ('albina.glick@epita.fr', 'DT_RDBMS', 'grade_score')
+
+
+select s.student_epita_email ,g.grade_course_code_ref, sum(e.exam_weight * g.grade_score)/sum(e.exam_weight) from grades g 
+inner join exams e on g.grade_course_code_ref = e.exam_course_code
+inner join students s on g.grade_student_epita_email_ref =s.student_epita_email 
+where s.student_epita_email ='kallie.blackwood@epita.fr' 
+group by g.grade_course_code_ref, s.student_epita_email  
 
 
 4 - /*Get the students with the top 5 scores for specific course*/
-
+with total_grade_course as (
+	select c.contact_first_name, c.contact_last_name ,g.grade_course_code_ref, sum(e.exam_weight * g.grade_score)/sum(e.exam_weight) as total_grade,
+	rank() over (partition by g.grade_course_code_ref order by sum(e.exam_weight * g.grade_score)/sum(e.exam_weight) desc) as rnk
+	from grades g 
+	inner join exams e on g.grade_course_code_ref = e.exam_course_code
+	inner join students s on g.grade_student_epita_email_ref = s.student_epita_email
+	inner join contacts c on s.student_contact_ref = c.contact_email 
+	group by g.grade_course_code_ref, c.contact_first_name, c.contact_last_name
+)
+select contact_first_name, contact_last_name, grade_course_code_ref, total_grade, rnk
+from total_grade_course
+where rnk <=5 and grade_course_code_ref ='DT_RDBMS'
 
 5 - /*Get the students with the top 5 scores for specific course
 per rank*/
 
 
 6 - /*Get the Class average for a course*/
+  
+select g.grade_course_code_ref, (sum(e.exam_weight * g.grade_score)/sum(e.exam_weight)::float) as class_average
+from grades g inner join exams e on g.grade_course_code_ref = e.exam_course_code
+inner join students s on g.grade_student_epita_email_ref =s.student_epita_email 
+group by g.grade_course_code_ref
 
 --bonuses:
 1 - /*Get a student full report of grades and attendances*/
